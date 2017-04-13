b29bd0b1e1771dbc94949907e82a8df41f618dfa
Patt-tom McDonnell <pt@midrive.com>
2017-04-13 14:57:57 +0100

    Add options for reverse geocoding

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function geocode(options){
 
   var query = {};
 
+  if(!options){
+    throw new Error('Must supply an options object');
+  }
+
   if(options.address){
     query.address = options.address;
   }
@@ -26,12 +30,29 @@ function geocode(options){
     query.bounds = northeast + '|' + southwest;
   }
 
-  if(options.components){
+  if(options.components || options.result_type){
+    if(options.result_type){
+      options.components = options.result_type
+    }
     query.components = [];
     for(var i in options.components){
       query.components.push([i, options.components[i]].join(':'))
     }
     query.components = query.components.join('|')
+  }
+
+  if(options.latlng){
+    if(options.place_id){
+      throw new Error('Cannot supply latlng and place_id in a single query')
+    }
+    query.latlng = options.latlng
+  }
+
+  if(options.place_id){
+    if(options.latlng){
+      throw new Error('Cannot supply latlng and place_id in a single query')
+    }
+    query.place_id = options.place_id
   }
 
   request.get(baseUrl).query(query).end(function(err, response){
